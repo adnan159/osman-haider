a4f81cdfe9d48b691bea573427544ab16be7331f
Adnan <osmanhaider159@gmail.com>
2024-04-01 23:26:14 +0600

    vandor directory added to git

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'osman/haider',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '7dd0b4ab97b885b5a73ddfa280cc33e96a4ae78c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'osman/haider' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '7dd0b4ab97b885b5a73ddfa280cc33e96a4ae78c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
